1ba6e501b0c405b236348f9cc61207d48a0c2bc9
Yu Ting Wei <ytsimon2004@gmail.com>
2023-01-13 00:26:05 +0100

    local maxima roi

--- src/py2dccf/util.py
+++ src/py2dccf/util.py
@@ -1,11 +1,10 @@
 from pathlib import Path
-from typing import NamedTuple, Literal, get_args, Union
+from typing import NamedTuple, Literal, Union
 
 import cv2
 import numpy as np
-from scipy.ndimage import maximum_filter
 
-CHANNEL_TYPE = Literal['red', 'green', 'blue']
+ROI_CHANNEL_TYPE = Literal['red', 'green']
 
 
 class TifFile(NamedTuple):
@@ -13,6 +12,7 @@ class TifFile(NamedTuple):
     red_channel: np.ndarray
     green_channel: np.ndarray
     blue_channel: np.ndarray
+    """normally reference channel. i.e., DAPI"""
 
     @property
     def height(self) -> int:
@@ -22,31 +22,25 @@ class TifFile(NamedTuple):
     def width(self) -> int:
         return self.image.shape[1]
 
-    def generate_filtered_image(self, channel: CHANNEL_TYPE, kernal_size: int = 11):
+    def local_maxima_image(self, channel: ROI_CHANNEL_TYPE) -> np.ndarray:
         """
-        TODO try diplib https://stackoverflow.com/questions/56134035/how-to-make-ndimage-filters-maximum-filter-work-like-matlabs-imregionalmax-func
-        TODO use origin roi reader from fiji
 
         :param channel:
-        :param kernal_size:
         :return:
         """
-
+        from skimage.morphology import local_maxima
 
         if channel == 'red':
             image = self.red_channel
         elif channel == 'green':
             image = self.green_channel
-        elif channel == 'blue':
-            image = self.blue_channel
         else:
             raise ValueError(f'{channel} unknown')
 
         if np.sum(image) == 0:
             return np.zeros_like(image, dtype=np.uint8)
         else:
-            return np.array(image == maximum_filter(image, kernal_size))
-
+            return local_maxima(image)
 
 def load_image_file(file: Union[Path]) -> TifFile:
     img = cv2.imread(str(file))
@@ -59,11 +53,11 @@ if __name__ == '__main__':
     file = Path('/Users/simon/code/Analysis/test_mat/YW043_2_2_b_selection_processed_transformed.tif')
     tif = load_image_file(file)
 
-    filtered = tif.generate_filtered_image('green', kernal_size=5)
+    filtered = tif.local_maxima_image('red')
 
     import matplotlib.pyplot as plt
     fig, ax = plt.subplots(1, 2)
-    ax[0].imshow(tif.green_channel)
+    ax[0].imshow(tif.red_channel)
     ax[1].imshow(filtered)
 
     ax[1].sharex(ax[0])
