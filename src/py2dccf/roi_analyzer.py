17fff46c641263d79c251009ff63737c4e28f4d3
Yu Ting Wei <ytsimon2004@gmail.com>
2023-01-13 11:00:07 +0100

    tmp save

--- src/py2dccf/roi_analyzer.py
+++ src/py2dccf/roi_analyzer.py
@@ -1,12 +1,15 @@
 import re
 from pathlib import Path
-from typing import List, Union
+from typing import List, Union, Tuple, Literal
 
 import numpy as np
+import pandas as pd
 from PIL import Image
 from scipy.io import loadmat
 
-from src.py2dccf.util import load_image_file
+from src.py2dccf.util import load_image_file, load_structure_tree, PLANE_TYPE, allenccf_bregma
+
+ROI_CHANNEL = Literal['r', 'g', 'rg']
 
 
 class ROIAnalyzer:
@@ -15,15 +18,25 @@ class ROIAnalyzer:
     def __init__(self,
                  path: Path,
                  ref_path: Path,
-                 n_ch: int):
+                 roi_channel: ROI_CHANNEL,
+                 plane: PLANE_TYPE):
         """
 
         :param path: transformation folder
+        :param ref_path
+        :param roi_channel
+        :param plane
         """
 
         self.path = path
         self.ref_path = ref_path
-        self.n_ch = n_ch
+        self.roi_channel = roi_channel
+        self.plane = plane
+
+        self.roi_annotation: dict = dict(avIndex=[], name=[], acronym=[])
+
+        self._bregma = allenccf_bregma()
+        self._atlas_res = 0.01  # mm
 
     @property
     def data(self) -> List[Path]:  # TODO sorted?
@@ -31,26 +44,117 @@ class ROIAnalyzer:
 
     @property
     def images(self) -> List[Path]:
-        return list(self.path.glob('*transformation.tif'))
+        return list(self.path.glob('*transformed.tif'))
 
     @property
-    def annotation_file(self) -> Path:
-        return self.ref_path / 'annotation_volume_10um_by_index.npy'
+    def annotation_file(self) -> np.ndarray:
+        return np.load(self.ref_path / 'annotation_volume_10um_by_index.npy')
 
     @property
-    def structure_tree(self) -> Path:
-        return self.ref_path / 'structure_tree_safe_2017.csv'
+    def av_plot(self) -> np.ndarray:
+        """plane orientation"""
+        av = self.annotation_file
+        if self.plane == 'coronal':
+            return av  # (1320, 800, 1140)
+        elif self.plane == 'sagittal':
+            x, y, z = av.shape
+            return av.reshape((z, y, x))
+        elif self.plane == 'transverse':
+            x, y, z = av.shape
+            return av.reshape((y, z, x))
+        else:
+            raise ValueError(f'{self.plane} unknown')
+
+    @property
+    def structure_tree(self) -> pd.DataFrame:
+        return load_structure_tree(self.ref_path / 'structure_tree_safe_2017.csv')
 
     @property
     def save_folder(self) -> Path:
         return self.path / 'labelled_region'
 
-    def analysis(self):
-        table = np.zeros(len(self.images), self.n_ch)
+    def _foreach_channel_rois(self,
+                              rois: np.ndarray,
+                              slice_angle: np.ndarray,
+                              slice_num: int) -> pd.DataFrame:
+        """
+
+        :param rois: binarized image array of roi selection
+        :return:
+        """
+        _, *ref_size = self.av_plot.shape
+        if rois.shape != tuple(ref_size):
+            raise RuntimeError('roi image is not the right size')
+
+        pixel_row, pixel_col = np.where(rois > 0)
+
+        rois_loc = np.zeros((len(pixel_row), 3))
+
+        offset_map = self._get_offset_map(slice_angle, ref_size)
+        bregma = self._bregma
+        res = self._atlas_res
+
+        for i, (pr, pc) in enumerate(zip(pixel_row, pixel_col)):
+            offset = offset_map[pc, pr]
+
+            if self.plane == 'coronal':
+                ap = -(slice_num - bregma[0] + offset) * res
+                dv = (pr - bregma[1]) * res
+                ml = (pc - bregma[2]) * res
+            elif self.plane == 'sagittal':
+                ap = -(pc - bregma[0]) * res
+                dv = (pr - bregma[1]) * res
+                ml = -(slice_num - bregma[2] + offset) * res
+            elif self.plane == 'transverse':
+                ap = -(pc - bregma[0]) * res
+                dv = -(slice_num - bregma[1] + offset) * res
+                ml = (pr - bregma[2]) * res
+            else:
+                raise ValueError(f'unknown {self.plane}')
+
+            rois_loc[i, :] = (ap, dv, ml)
+
+            # finally, find the annotation, name, and acronym of the current ROI pixel
+            annot = self.av_plot[int(slice_num + offset), pr, pc]
+            self.roi_annotation['avIndex'].append(annot)
+            self.roi_annotation['name'].append(self.structure_tree['safe_name'][annot])
+            self.roi_annotation['acronym'].append(self.structure_tree['acronym'][annot])
+
+        self.roi_annotation['AP_location'] = rois_loc[:, 0]
+        self.roi_annotation['DV_location'] = rois_loc[:, 1]
+        self.roi_annotation['ML_location'] = rois_loc[:, 2]
+
+        return pd.DataFrame.from_dict(self.roi_annotation)
+
+    @staticmethod
+    def _get_offset_map(slice_angle: np.ndarray, ref_size: Tuple[int, int]) -> np.ndarray:
+        """
+        Generate offset map (for third dimension of a tilted slice)
+        from allenCCF/Browsing Functions/get_offset_map.m
+
+        ..seealso::
+
+            :func:`matlab.img.get_offset_map.m`
+
+        :param slice_angle: (2,)
+        :param ref_size: (height, width)
+        :return:
+            TODO
+        """
+        angle_ap = int(slice_angle[0])
+        angle_ml = int(slice_angle[1])
+
+        ap_frame = np.round(np.linspace(-angle_ap, angle_ap, ref_size[0])).astype(int)
+        ml_frame = np.round(np.linspace(-angle_ml, angle_ml, ref_size[1])).astype(int)
+
+        return ap_frame[None, :] + ml_frame[:, None]
+
+    def run_analysis(self):
+        # table = np.zeros((len(self.images), len(self.roi_channel)))  # TODO check
         for i, img in enumerate(self.images):
             img_name = self.images[i].name
             tmp = [pattern.start() for pattern in re.finditer('_', img_name)]
-            print(f'{i} of {len(self.images)}')
+            print(f'{i + 1} of {len(self.images)}')
             data = self.data[i]
 
             # load transformed slice image and transformed matrix
@@ -58,9 +162,26 @@ class ROIAnalyzer:
             mat = loadmat(data, squeeze_me=True, struct_as_record=False)['save_transform']
 
             # get the position within the atlas data of the transformed slice
-            slice_num = mat.allen_location[0]
-            slice_angle = mat.allen_location[1]
+            slice_num: int = mat.allen_location[0]
+            slice_angle: np.ndarray = mat.allen_location[1]
 
-            roi_r = slice_img.red_channel
-            roi_g = slice_img.green_channel
             blue = slice_img.blue_channel
+
+            if self.roi_channel == 'r':
+                rois = slice_img.local_maxima_image('red')
+
+            elif self.roi_channel == 'g':
+                rois = slice_img.local_maxima_image('green')
+            else:
+                raise NotImplementedError('')
+
+            df = self._foreach_channel_rois(rois, slice_angle, slice_num)
+            print(df.to_markdown())
+
+
+if __name__ == '__main__':
+    path = Path('/Users/simon/code/Analysis/test_mat')
+    ref = Path('/Users/simon/code/Analysis/histology/reference-atlas-files')
+
+    ra = ROIAnalyzer(path, ref, 'r', 'coronal')
+    ra.run_analysis()
--- src/py2dccf/util.py
+++ src/py2dccf/util.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from typing import NamedTuple, Literal, Union
-
+import pandas as pd
 import cv2
 import numpy as np
 
 ROI_CHANNEL_TYPE = Literal['red', 'green']
+PLANE_TYPE = Literal['coronal', 'sagittal', 'transverse']
 
 
 class TifFile(NamedTuple):
@@ -42,11 +43,103 @@ class TifFile(NamedTuple):
         else:
             return local_maxima(image)
 
+
 def load_image_file(file: Union[Path]) -> TifFile:
     img = cv2.imread(str(file))
     b, g, r = cv2.split(img)
 
     return TifFile(img, r, g, b)
+
+
+def load_structure_tree(fpath: Path) -> pd.DataFrame:
+    """
+    todo allenCCF/Browsing Functions/loadStructureTree.m further?
+
+    ..seealso::
+
+        :func:`matlab.img.loadStructureTree.m`
+
+    :param fpath: csv
+    :return:
+    """
+    if '2017' in fpath.name:
+        return _load_structure_tree_2017(fpath)
+    else:
+        return _load_structure_tree_old(fpath)
+
+
+def _load_structure_tree_2017(fpath: Path) -> pd.DataFrame:
+    """
+    parse the 'structure_tree_safe_2017.csv' from allenCCF
+
+    :param fpath: 'structure_tree_safe_2017.csv' path
+    :return: data frame with header
+        id: int
+        atlas_id: int, -2 for empty
+        name: str
+        acronym: str
+        st_level: str
+        ontology_id: int
+        hemisphere_id: int
+        weight: int
+        parent_structure_id: int, -1 for root
+        depth: int
+        graph_id: int
+        graph_order: int
+        structure_id_path: tuple[int]
+        color_hex_triplet: str
+        neuro_name_structure_id: str
+        neuro_name_structure_id_path: str
+        failed: str
+        sphinx_id: int
+        structure_name_facet: int
+        failed_facet: int
+        safe_name: str
+    """
+    return pd.read_csv(
+        fpath, sep=',',
+        converters=dict(
+            atlas_id=lambda it: int(float(it)) if len(it) else -2,
+            parent_structure_id=lambda it: int(float(it)) if len(it) else -1,
+            structure_id_path=lambda it: tuple(map(int, it[1:-1].split('/'))),
+            st_level=str,
+            neuro_name_structure_id=str,
+            neuro_name_structure_id_path=str,
+        )
+    )
+
+
+def _load_structure_tree_old(fpath: Path) -> pd.DataFrame:
+    """
+
+    :param fpath:
+    :return: data frame with header
+        acronym: str
+        id: int
+        name: str
+        structure_id_path: tuple[int]
+        parent_structure_id: int, -1 for root
+        safe_name: str, as same as name
+    """
+    ret = pd.read_csv(
+        fpath, sep=',',
+        converters=dict(
+            structure_id_path=lambda it: tuple(map(int, it[1:-1].split('/'))),
+            parent_structure_id=lambda it: int(float(it)) if len(it) else -1
+        )
+    )
+    ret['safe_name'] = ret['name']
+    return ret
+
+
+def allenccf_bregma() -> np.ndarray:
+    """
+    Return coordinates of bregma in allen CCF 10um volume coordinates
+    from allenCCF/Browsing Functions/allenCCFbregma.m
+
+    :return: AP, DV, ML(LR) coordinate
+    """
+    return np.array([540, 0, 570])
 
 
 if __name__ == '__main__':
@@ -56,6 +149,7 @@ if __name__ == '__main__':
     filtered = tif.local_maxima_image('red')
 
     import matplotlib.pyplot as plt
+
     fig, ax = plt.subplots(1, 2)
     ax[0].imshow(tif.red_channel)
     ax[1].imshow(filtered)
